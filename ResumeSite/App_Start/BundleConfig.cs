e8f2977b1e5446e7bc7eb54393b619a96545b896
Joseph Spalding <joseph.spalding@ZIRMED.COM>
2018-09-18 12:56:40 -0400

    Add project files.

---
+++ ResumeSite/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ResumeSite.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+
+            //this bundle will be for custom CSS
+            bundles.Add(new StyleBundle("~/bundles/sitestyles").Include(
+                "~/Content/Site.css",
+                "~/Content/Error.css"
+                ));
+            bundles.Add(new ScriptBundle("bundlepath").Include(
+                "realpath"
+                ));
+        }
+    }
+}
---
+++ ResumeSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResumeSite.MvcApplication" Language="C#" %>
