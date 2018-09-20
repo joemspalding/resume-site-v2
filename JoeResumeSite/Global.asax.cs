93c66ee955dc1b6cfd05b9ab22ddfbf55b5280ca
Joe Spalding <boloneyshinobi@gmail.com>
2018-09-20 16:28:47 -0400

    add custom error pages and cleaned up some styling

--- JoeResumeSite/Global.asax.cs
+++ JoeResumeSite/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using JoeResumeSite.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -17,29 +12,6 @@ namespace JoeResumeSite
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-            Server.ClearError();
-            var routeData = new RouteData();
-            routeData.Values["controller"] = "Error";
-
-            if ((Context.Server.GetLastError() is HttpException) && ((Context.Server.GetLastError() as HttpException).GetHttpCode() != 404))
-            {
-                routeData.Values["action"] = "InternalError";
-            }
-            else
-            {
-                routeData.Values["action"] = "NotFound";
-            }
-            Response.TrySkipIisCustomErrors = true;
-            IController errorsController = new ErrorController();
-            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
-            var rc = new RequestContext(wrapper, routeData);
-            errorsController.Execute(rc);
-
-            Response.End();
         }
     }
 }
