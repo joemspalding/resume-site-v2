e42d6e97184c0ca2b99e76a7b27a0b7143d77941
Joe Spalding <boloneyshinobi@gmail.com>
2018-09-24 15:03:40 -0400

    fixing font-awesome - do at home

--- JoeResumeSite/App_Start/BundleConfig.cs
+++ JoeResumeSite/App_Start/BundleConfig.cs
@@ -50,10 +50,9 @@ namespace JoeResumeSite
                         ));
 
             // please work for daddy..........
-            bundles.Add(new StyleBundle("~/bundles/font-awesome.css", @"https://use.fontawesome.com/releases/v5.3.1/css/brands.css")
+            bundles.Add(new StyleBundle("~/bundles/font-awesome")//, @"https://use.fontawesome.com/releases/v5.3.1/css/brands.css")
                 .Include(
-                         "~/Content/fontawesome.min.css",
-                         new CssRewriteUrlTransform()
+                         "~/Content/font-awesome.css"
                          ));
 
             bundles.Add(new StyleBundle("~/bundles/css")
